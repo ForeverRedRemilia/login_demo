ae8445bd4299c2186ab47d7cb71af462972879b4
ForeverRedRemilia <foreverredremilia@touhou.org>
2020-09-18 16:56:17 +0800

    初始化

---
+++ src/components/index/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="header-div">
+      <span class="header-span"><a><i class="el-icon-question"></i>帮助</a></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+  .header-div {
+    /*background-size: 4096px;*/
+    height: 50px;
+    margin: 0 auto;
+    background-color: #239676;
+  }
+
+  .header-span {
+    font-size: 24px;
+    color: #252525;
+  }
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Header></Header>
+    fuck you
+    <HelloWorld></HelloWorld>
+    fuck you
+  </div>
+</template>
+
+<script>
+import HelloWorld from '../components/HelloWorld'
+import Header from '../components/index/Header'
+export default {
+  name: 'Index',
+  components: {
+    Header,
+    HelloWorld
+  }
+}
+</script>
+
+<style scoped>
+.root-div{
+  height: 100%;
+  width: 100%;
+}
+</style>
